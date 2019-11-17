1f9c83ec8160773bff2de6e0c5dc4b79a54125c4
Pavel Bondar <ivizey@gmail.com>
2019-11-17 18:39:14 +0200

    Initial Commit

---
+++ UIVisualEffectView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIVisualEffectView
+//
+//  Created by Pavel Bondar on 17.11.2019.
+//  Copyright © 2019 Pavel Bondar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
